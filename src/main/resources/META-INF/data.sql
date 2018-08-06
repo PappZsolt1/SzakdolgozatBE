e82bf42a5486e10fb83daa31c9e18ef78b02d90d
Papp Zsolt <pappzsolt3@gmail.com>
2018-08-06 04:26:28 +0200

    fix

--- src/main/resources/META-INF/data.sql
+++ src/main/resources/META-INF/data.sql
@@ -1,4 +1,1 @@
--- INSERT INTO errorreport VALUES (1, "Hiba.", false, "2018. 05. 02. 22:08:32", null)
--- #comment
-
-INSERT INTO ASD VALUES (12, "aaa");
+INSERT INTO errorreport VALUES (1, "Hiba.", false, "2018. 05. 02. 22:08:32", null)
