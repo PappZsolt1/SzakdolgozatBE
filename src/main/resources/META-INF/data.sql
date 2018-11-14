770b286c2f2f3a95d835558fc150eadb940357d3
Papp Zsolt <pappzsolt3@gmail.com>
2018-11-14 05:01:02 +0100

    rules

--- src/main/resources/META-INF/data.sql
+++ src/main/resources/META-INF/data.sql
@@ -11,4 +11,8 @@ INSERT INTO AgeClassification VALUES (1, "12+")
 INSERT INTO AgeClassification VALUES (2, "16+")
 INSERT INTO AgeClassification VALUES (3, "18+")
 
-INSERT INTO errorreport VALUES (1, "Hiba.", false, "2018. 05. 02. 22:08:32", null)
+INSERT INTO Rules VALUES (1, "Hozzászólások írása során tilos trágár, rasszista, gyűlöletkeltő\r\nszavakat illetve kifejezéseket használni. Amennyiben ez megtörténik,\r\na hozzászólás tartalma moderátorok által törölve lesz!\r\n\r\nÚj témák létrehozásakor a hozzászólásokra vonatkozó szabályokat\r\nbe kell tartani, valamint a témának az oldal tartalmához kapcsolódnia kell,\r\nilletve olyan témát nem szabad létrehozni, amilyen már létezik.\r\nHa ezen szabályok nincsenek betartva, a téma moderátorok által törölve lesz!\r\n\r\nA szabályok nem ismerete nem mentesít azok betartása alól!")
+
+INSERT INTO ErrorReport VALUES (1, "Hiba.", false, "2018. 05. 02. 22:08:32", null)
+
+INSERT INTO Article VALUES (10, "aaa\r\nbbb\r\n\r\nccc", null, false, true, "test")
