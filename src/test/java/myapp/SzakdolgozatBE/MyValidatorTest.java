0f59bb90da977eeaac91ffb17de90fe99aec19b5
Papp Zsolt <pappzsolt3@gmail.com>
2018-12-07 21:23:53 +0100

    myval test

--- src/test/java/myapp/SzakdolgozatBE/MyValidatorTest.java
+++ src/test/java/myapp/SzakdolgozatBE/MyValidatorTest.java
@@ -1,120 +1,79 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package myapp.SzakdolgozatBE;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author Gep
- */
 public class MyValidatorTest {
-    
-    public MyValidatorTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
-    /**
-     * Test of validateText method, of class MyValidator.
-     */
+    MyValidator val = new MyValidator();
+    
     @Test
     public void testValidateText() {
-        System.out.println("validateText");
-        String text = "";
-        int max = 0;
-        MyValidator instance = new MyValidator();
-        boolean expResult = false;
-        boolean result = instance.validateText(text, max);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(val.validateText("", 100), false);
+        assertEquals(val.validateText("a", 100), true);
+        assertEquals(val.validateText(" aaaa aa", 100), false);
+        assertEquals(val.validateText("sds s ds ", 100), false);
+        assertEquals(val.validateText("asdas sada\n   asd", 100), true);
+        assertEquals(val.validateText("\na asd", 100), false);
+        assertEquals(val.validateText("\ra a ds", 100), false);
+        assertEquals(val.validateText("\taa as", 100), false);
+        assertEquals(val.validateText("asdas\n", 100), false);
+        assertEquals(val.validateText("as sa s\t", 100), false);
+        assertEquals(val.validateText("asda\r", 100), false);
+        assertEquals(val.validateText("asda asdasdasd", 5), false);
     }
 
-    /**
-     * Test of validateNumber method, of class MyValidator.
-     */
     @Test
     public void testValidateNumber() {
-        System.out.println("validateNumber");
-        int number = 0;
-        int min = 0;
-        int max = 0;
-        MyValidator instance = new MyValidator();
-        boolean expResult = false;
-        boolean result = instance.validateNumber(number, min, max);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(val.validateNumber(10, 10, 10), true);
+        assertEquals(val.validateNumber(10, 1, 10), true);
+        assertEquals(val.validateNumber(10, 10, 100), true);
+        assertEquals(val.validateNumber(5, 6, 10), false);
+        assertEquals(val.validateNumber(213, 1, 210), false);
     }
 
-    /**
-     * Test of validateDate method, of class MyValidator.
-     */
     @Test
     public void testValidateDate() {
-        System.out.println("validateDate");
-        String date = "";
-        int min = 0;
-        int max = 0;
-        MyValidator instance = new MyValidator();
-        boolean expResult = false;
-        boolean result = instance.validateDate(date, min, max);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(val.validateDate("2000. 01. 01.", 1750, 2100), true);
+        assertEquals(val.validateDate("2100. 12. 30.", 1750, 2100), true);
+        assertEquals(val.validateDate("2101. 02. 05.", 1750, 2100), false);
+        assertEquals(val.validateDate("1750. 03. 20.", 1750, 2100), true);
+        assertEquals(val.validateDate("1749. 07. 03.", 1750, 2100), false);
+        assertEquals(val.validateDate("2200. 01. 01.", 1750, 2100), false);
+        assertEquals(val.validateDate("1700. 01. 01.", 1750, 2100), false);
+        assertEquals(val.validateDate("2000.01. 01.", 1750, 2100), false);
+        assertEquals(val.validateDate("2000. 13. 01.", 1750, 2100), false);
+        assertEquals(val.validateDate("2000. 02. 30.", 1750, 2100), false);
     }
 
-    /**
-     * Test of validateLength method, of class MyValidator.
-     */
     @Test
     public void testValidateLength() {
-        System.out.println("validateLength");
-        String length = "";
-        MyValidator instance = new MyValidator();
-        boolean expResult = false;
-        boolean result = instance.validateLength(length);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(val.validateLength("2 óra 21 perc"), true);
+        assertEquals(val.validateLength("2 óra 5 perc"), true);
+        assertEquals(val.validateLength("11 óra 0 perc"), true);
+        assertEquals(val.validateLength("89 óra 3 perc"), true);
+        assertEquals(val.validateLength("8 perc"), true);
+        assertEquals(val.validateLength("54 perc"), true);
+        assertEquals(val.validateLength("100 óra 21 perc"), true);
+        assertEquals(val.validateLength("101 óra 21 perc"), false);
+        assertEquals(val.validateLength("500 óra 21 perc"), false);
+        assertEquals(val.validateLength("2 óra 61 perc"), false);
+        assertEquals(val.validateLength("1 óra -2 perc"), false);
+        assertEquals(val.validateLength("2óra 35 perc"), false);
+        assertEquals(val.validateLength(" 45 perc"), false);
+        assertEquals(val.validateLength("3perc"), false);
+        assertEquals(val.validateLength("2 óra 21 per"), false);
     }
 
-    /**
-     * Test of validateSize method, of class MyValidator.
-     */
     @Test
     public void testValidateSize() {
-        System.out.println("validateSize");
-        int size = 0;
-        MyValidator instance = new MyValidator();
-        boolean expResult = false;
-        boolean result = instance.validateSize(size);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(val.validateSize(10), true);
+        assertEquals(val.validateSize(20), true);
+        assertEquals(val.validateSize(30), true);
+        assertEquals(val.validateSize(0), false);
+        assertEquals(val.validateSize(12), false);
+        assertEquals(val.validateSize(52), false);
+        assertEquals(val.validateSize(-10), false);
+        assertEquals(val.validateSize(110), false);
     }
-    
 }
